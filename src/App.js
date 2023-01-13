c7b4450b524aa4b395b98604357949878bb456df
orpichardo <orlandopichardo809@gmail.com>
2023-01-13 10:42:06 -0500

    first commit fetch done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const [fruits, setFruits] = useState([])
+  const [fruit, setFruit] = useState({})
+  
+
+  useEffect(() => {
+    apiCall()
+  }, [])
+
+  function apiCall() {
+    fetch("https://project2-production.up.railway.app/fruits")
+      .then((res) => res.json())
+      .then((data) => setFruits(data.results))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
